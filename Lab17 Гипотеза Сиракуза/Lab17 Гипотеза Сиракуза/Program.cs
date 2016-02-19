24b669786dc4a612f14f0d6e447c7b4ad9076985
Pin131GasanowO <cherep.monolit@yandex.ru>
2016-02-19 12:22:16 +0300

    Программа "Гипотеза Сиракуза"

---
+++ Lab17 Гипотеза Сиракуза/Lab17 Гипотеза Сиракуза/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17_Гипотеза_Сиракуза
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.Write("Введите число: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                if (x % 2 == 0)
+                {
+                    x = x / 2;
+                }
+                else
+                {
+                    x = (3 * x + 1) / 2;
+                }
+            }
+            while (x != 1);
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
