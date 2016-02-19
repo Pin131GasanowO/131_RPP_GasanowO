934d3b6e93af5df6d5ddfe5ee0c1e00b8c4284d0
Pin131GasanowO <cherep.monolit@yandex.ru>
2016-02-19 12:19:45 +0300

    Программа "Таблица умножения"

---
+++ Lab16 Таблица умножения/Lab16 Таблица умножения/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab16_Таблица_умножения
+{
+    class Program
+    {
+        private const int n = 9;
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            Console.Write("Pifagor's table\n");
+            for (i = 1; i <= n; i++) //внешний цикл
+            {
+                Console.Write(" \n");
+                for (j = 1; j <= n; j++) //внутрненний цикл
+                {
+                    Console.Write(j * i);
+                    Console.Write(" ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
