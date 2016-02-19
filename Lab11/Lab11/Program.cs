1f68b34fcc5cf972ec5bcd97172f957b1e2be31d
Pin131GasanowO <cherep.monolit@yandex.ru>
2016-02-19 12:01:23 +0300

    Программа "Пароль"

---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            int i;
+            Console.Write("Скажи пароль и проходи: ");
+            for (i = 0; i < 3; i++)
+            {
+                x = (Console.ReadLine());
+                if (x == "time is dead")
+                {
+                    Console.Write("Можешь проходить сталкер. Удачной охоты. ");
+                }
+                else
+                {
+                    Console.Write("Ты ошибся. Пробуй снова! ");
+                    Console.Write("До провала осталось: ");
+                    Console.Write(3 - i);
+                    Console.Write(" попытки. ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
