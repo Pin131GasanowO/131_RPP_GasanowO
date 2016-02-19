fb97b942464db5a0d7a91f7e73524836a3537219
Pin131GasanowO <cherep.monolit@yandex.ru>
2016-02-19 12:15:40 +0300

    Программа "Делители числа в порядке убывания"

---
+++ Lab15 Делители числа в порядке убывания/Lab15 Делители числа в порядке убывания/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15_Делители_числа_в_порядке_убывания
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, a;
+            Console.Write("Введите число ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            for (a = x; a <= x; a--)
+            {
+                if (a > 0)
+                {
+                    if ((x % a) == 0)
+                        Console.WriteLine("Делитель числа {0} : {1}", x, a);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
