29183c699997263baeeb7dab2e50a92ad1c74fdf
Pin131GasanowO <cherep.monolit@yandex.ru>
2016-02-19 12:10:00 +0300

    Программа "Максимальное из вводимых чисел"

---
+++ Lab13 Максимальное из вводимых чисел/Lab13 Максимальное из вводимых чисел/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13_Максимальное_из_вводимых_чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+            string[] strs = str.Split(' ');
+            int max = int.Parse(strs[0]);
+            for (int i = 1; i < strs.Length; i++)
+            {
+                int cur = int.Parse(strs[i]);
+                if (max < cur) max = cur;
+            }
+            Console.WriteLine("Максимум: {0}", max);
+            Console.ReadKey();
+        }
+    }
+} 
