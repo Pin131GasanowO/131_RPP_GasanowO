4e4cd7dc587c1b5b86d03be47eaff3e8425f7e7e
Pin131GasanowO <cherep.monolit@yandex.ru>
2016-02-19 12:12:59 +0300

    Программа "Сумма вводимых чисел"

---
+++ Lab14 Сумма всех вводимых чисел/Lab14 Сумма всех вводимых чисел/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14_Сумма_всех_вводимых_чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input, sum = 0;
+            while ((input = int.Parse(Console.ReadLine())) != 0) sum += input;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
