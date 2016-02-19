9cb822b599f6e30d04edc92831e83f04b291021a
Pin131GasanowO <cherep.monolit@yandex.ru>
2016-02-19 12:06:58 +0300

    Программа "Угадайка с оппонентом"

---
+++ Lab12 Угадайка с оппонентом/Lab12 Угадайка с оппонентом/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12_Угадайка_с_оппонентом
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            while (true)
+            {
+
+                Console.WriteLine("Введите число");
+
+                int y = int.Parse(Console.ReadLine());
+
+                if (y > x + 50)
+                {
+                    Console.WriteLine("Ваше число намного больше");
+
+                }
+                else if (y < x - 50)
+                {
+                    Console.WriteLine("Ваше число намного меньше");
+                }
+                else if (y > x)
+                {
+                    Console.WriteLine("Ваше число больше");
+                }
+
+                else if (y < x)
+                {
+                    Console.WriteLine("Ваше число меньше");
+                }
+                else
+                {
+                    Console.WriteLine("!!!АЙ МАЛАДЦА!!!");
+
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
