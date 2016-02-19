dbd52ea3807421b982f88e996cc9be24ffe313c2
Pin131GasanowO <cherep.monolit@yandex.ru>
2016-02-19 12:04:28 +0300

    Программа "Угадайка с компьютером"

---
+++ Lab12 Угадайка с компьютером/Lab12 Угадайка с компьютером/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12_Угадайка_с_компьютером
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите начало интервала: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите конец интервала: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Random rand = new Random();
+
+            int A = rand.Next(x, y);
+            int l = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите число: ");
+
+                int B = int.Parse(Console.ReadLine());
+
+                l++;
+                if (B == A)
+                {
+                    Console.WriteLine("Вы угадали с {0} попытки: ", l);
+                    break;
+                }
+
+                else if (B > A)
+                {
+                    Console.WriteLine("Ваше число больше");
+                }
+                else
+                {
+                    Console.WriteLine("Ваше число меньше");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
